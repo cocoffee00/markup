66e47289d96c1044c2c891f1f37431b4096be705
cocoffee <cocoffee724@gmail.com>
2022-02-11 00:09:12 +0900

    [create] gallery 기본예제

---
+++ b_step_04/js/src/b_08_gallery_02.js
@@ -0,0 +1,43 @@
+// b_08_gallery_02.js
+
+(function($){
+//jQuery
+
+
+// 변수
+  var galleryArea = $('.gallery_area2');
+
+  var galContent = galleryArea.find('.gallery_content');
+  var galImg = galContent.find('img');
+
+  var gaiUl = galleryArea.find('.gallery_list');
+  var galLi = gaiUl.find('li');
+  var galBtn = galLi.children('button');
+
+  var imgFn = function(n){
+    galImg.eq(n).addClass('on');
+    galImg.eq(n).siblings().removeClass('on');
+  };
+  var btnFn = function(n){
+    galLi.eq(n).addClass('on');
+    galLi.eq(n).siblings().removeClass('on');
+  };
+
+  
+// 이벤트
+
+  galBtn.on('click',function(e){
+    e.preventDefault();
+      var i = $(this).parent().index();
+      imgFn(i);
+      btnFn(i);
+  });
+
+
+
+
+
+
+
+
+})(jQuery);
