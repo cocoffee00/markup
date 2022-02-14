4198e27c851c970966c5b0559b2b37efa1bc1b18
cocoffee <cocoffee724@gmail.com>
2022-02-14 19:19:02 +0900

    [update] tab ment.js 기본예제

--- b_step_04/js/src/b_11_tab_menu.js
+++ b_step_04/js/src/b_11_tab_menu.js
@@ -12,10 +12,43 @@ $.ajax({
   context :document.body
 }).done(function(data){
   var dataFile = data;
-  console.log(dataFile);
+  // console.log(dataFile);
+
+  var i =0;
+  var len = dataFile.length;
+  var tabTitle = [];
+
+  for( ; i < len ; i+=1 ){
+    tabTitle.push( dataFile[i].type);
+  }
+  // console.log(tabTitle);
+
+  //________________________________________tab menu title 설정
+
+  // 배열.filter(function(data,index){});
+  // indexOf() : 문자열/배열 에서 작성한 요소와 일치하는 순서를 파악
+
+
+  // 중복되는 배열의 값을 정리해주는 기능
+   var tabTitleCk = tabTitle.filter(function(data,index){
+      // console.log(data,index);
+      // return data === 'coffee';
+      return tabTitle.indexOf(data) === index;
+    });
+
+    //var idx = tabTitle.indexOf('tea'); // <-dataFile안에 tea가 몇번째에서 처음 시작하는지 알려줌 (ex:16 ->16번부터 tea가 시작됨)
+    //console.log(idx);
+    console.log(tabTitleCk);
+
+
+
+
+
+
+
+
+
 
   
 }); // $.ajax()
-
-
 })(jQuery);
